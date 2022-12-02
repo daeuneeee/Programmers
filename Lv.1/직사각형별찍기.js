d334a29e3900d0aa5fd8292125720c97ddf7b488
daeuneeee <dxexx2@gmail.com>
2022-12-02 22:48:59 +0900

    프로그래머스 직사각형 별찍기

---
+++ Lv.1/직사각형별찍기.js
@@ -0,0 +1,11 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  let result = "";
+  const n = data.split(" ");
+  const a = Number(n[0]),
+    b = Number(n[1]);
+  for (let i = 0; i < a; i++) {
+    result += "*";
+  }
+  console.log(`${result}\n`.repeat(b));
+});
