7c679e414866566d206810e17362a0d52706d8ec
daeuneeee <dxexx2@gmail.com>
2022-12-21 11:06:11 +0900

    프로그래머스 Lv.0 day5

--- Lv.0/day4/배열의평균값.js
+++ Lv.0/day4/배열의평균값.js
@@ -3,4 +3,4 @@ function solution(numbers) {
 }
 
 // reduce를 평소에 잘 안써보아서 써보았는데,,!
-// console.log를 reduce 내부에서 찍으면 "Cannot read properties of undefined at functionTestReport" 라는 에러가 뜬다 ㅈ
+// console.log를 reduce 내부에서 찍으면 "Cannot read properties of undefined at functionTestReport" 라는 에러가 뜬다
