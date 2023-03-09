0708ac6ef35ec119713a90ba5d385885fce480df
daeuneeee <dxexx2@gmail.com>
2023-03-09 16:09:27 +0900

    프로그래머스 Lv.1 행렬의덧셈

--- Lv.1/행렬의덧셈.js
+++ Lv.1/행렬의덧셈.js
@@ -9,3 +9,18 @@ function solution(arr1, arr2) {
   }
   return answer;
 }
+
+function solution(arr1, arr2) {
+  const result = new Array(arr1.length)
+    .fill(0)
+    .map(() => new Array(arr1[0].length).fill(0));
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr1[i].length; j++) {
+      result[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+  return result;
+}
+
+// 이중 빈배열을 메서드를 사용해서 만들고 그 안에 값을 넣어주는 방식으로 풀었다.
