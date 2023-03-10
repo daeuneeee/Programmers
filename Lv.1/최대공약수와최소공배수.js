6ba8dda670dfe511b358a1e164dd84de59f18785
daeuneeee <dxexx2@gmail.com>
2023-03-10 17:39:36 +0900

    프로그래머스 Lv.1 최대공약수와 최소공배수

--- Lv.1/최대공약수와최소공배수.js
+++ Lv.1/최대공약수와최소공배수.js
@@ -11,3 +11,21 @@ function solution(n, m) {
   gcd.push((n * m) / gcd);
   return gcd;
 }
+
+// 최대공약수 먼저 구해서 풀기
+
+function solution(n, m) {
+  const max = Math.max(n, m);
+  const min = Math.min(n, m);
+  let x = 0;
+
+  for (let i = max; i <= n * m; i += max) {
+    if (i % min === 0) {
+      x += i;
+      break;
+    }
+  }
+  return [(n * m) / x, x];
+}
+
+// 이번엔 최소공배수를 먼저 구해서 풀어봤지롱요 다음엔 While문 써서 풀어보는게 목표
