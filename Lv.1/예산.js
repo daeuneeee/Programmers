a403d8f1f44861405a9c81920a862f7e9a1fe36f
daeuneeee <dxexx2@gmail.com>
2023-03-14 13:58:35 +0900

    프로그래머스 Lv.1 예산

--- Lv.1/예산.js
+++ Lv.1/예산.js
@@ -9,3 +9,20 @@ function solution(d, budget) {
   }
   return result;
 }
+
+// for문
+
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+  let result = 0;
+
+  let i = 0;
+  if (d[0] > budget) return 0;
+  while (budget >= d[i] + result) {
+    result += d[i];
+    i++;
+  }
+  return i;
+}
+
+// while문 너무어렵당
