f637e4114334175d0fdcc0f4c9b577f31fa2233c
daeuneeee <dxexx2@gmail.com>
2023-03-17 15:21:27 +0900

    프로그래머스 Lv.1 최소 직사각형

---
+++ Lv.1/최소직사각형.js
@@ -0,0 +1,8 @@
+const num = [];
+function solution(sizes) {
+  sizes.map((card) => card.sort((a, b) => a - b));
+  return (
+    Math.max(...sizes.map((card) => card[0])) *
+    Math.max(...sizes.map((card) => card[1]))
+  );
+}
